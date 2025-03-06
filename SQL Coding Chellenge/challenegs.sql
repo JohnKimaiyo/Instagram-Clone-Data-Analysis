59893d9b92d5b292773329c3e224eaf73c00bf87
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2025-03-06 12:23:25 +0300

    final

--- SQL Coding Chellenge/challenegs.sql
+++
@@ -1,5 +0,0 @@
--- We want to reward our users who have been around the longest
-
-SELECT * FROM ig_clone.users
-ORDER BY created_at DESC
-LIMIT 5;
---
+++ SQL Coding Chellenge/challenges.sql
@@ -0,0 +1,13 @@
+-- We want to reward our users who have been around the longest
+
+SELECT * FROM ig_clone.users
+ORDER BY created_at DESC
+LIMIT 5;
+-- 
+/*What day of the week do most users register on?
+We need to figure out when to schedule an ad campgain*/
+SELECT
+ date_format(created_at,'%W') AS 'day of the week', COUNT(*) AS 'total registration'
+FROM users
+GROUP BY 1
+ORDER BY 2 DESC;
